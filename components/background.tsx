c58782883a9ca8f9da55d7534ab34ad4a1f939b9
Maria Paz <pazrodriguez612@gmail.com>
2024-10-25 01:25:44 -0700

    circle modification

--- components/background.tsx
+++ components/background.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import MovingCircles from './moving-circles';
+import CircleBlob from './circle-blob';
 export default function Background() {
    return (
       <div className='absolute inset-0 -z-10 overflow-hidden dark:bg-surface-mixed-100'>
-         <MovingCircles
-            color1='bg-indigo-400/80'
-            color2='bg-blue-300/80'
-            color3='bg-pink-300/80'
-         />
+         <div className='absolute top-32 w-full max-w-2xl xl:ml-48 z-10'>
+            <CircleBlob
+               color1='bg-indigo-400/80'
+               xPos='-left-4'
+               yPos='top-0'
+               xMdPos='md:-left-10'
+            />
+            <CircleBlob
+               color1='bg-blue-300/80'
+               xPos='right-16'
+               yPos='top-44'
+               xMdPos='md:-right-30'
+               delay='animation-delay-2000'
+            />
+            <CircleBlob
+               color1='bg-pink-300/80'
+               xPos='right-0'
+               yPos='top-8'
+               xMdPos='md:left-64'
+               delay='animation-delay-4000'
+            />
+         </div>
+
          {/* <MovingCircles
             color1='bg-indigo-400/80'
             color2='bg-blue-300/80'
---
+++ components/circle-blob.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface circleBlobProps {
+   color1: string;
+   xPos: string;
+   yPos: string;
+   xMdPos?: string;
+   delay?: string;
+}
+
+export default function CircleBlob({
+   color1,
+   xPos,
+   yPos,
+   xMdPos,
+   delay,
+}: circleBlobProps) {
+   return (
+      <div
+         className={`absolute ${xPos} ${yPos} ${xMdPos} circle-wh-72 md:circle-wh-112 ${color1} rounded-full mix-blend-multiply filter blur-xl animate-blob ${delay}`}
+      />
+      // <div
+      //    className={`absolute top-44 right-16 md:-right-20 circle-wh-72 md:circle-wh-112 ${color2} rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-2000`}
+      // />
+      // <div
+      //    className={`absolute top-8 right-0 md:left-64 circle-wh-72 md:circle-wh-112 ${color3} rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-4000`}
+      // />
+   );
+}
