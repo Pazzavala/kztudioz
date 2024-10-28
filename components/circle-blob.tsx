f6b90fe73c43bb00b499adb53e7b9e1574f4c49d
Maria Paz <pazrodriguez612@gmail.com>
2024-10-28 01:06:06 -0700

    circle comments removed

--- components/circle-blob.tsx
+++ components/circle-blob.tsx
@@ -19,11 +19,5 @@ export default function CircleBlob({
       <div
          className={`absolute ${xPos} ${yPos} ${xMdPos} circle-wh-72 md:circle-wh-112 ${color1} rounded-full mix-blend-multiply filter blur-3xl animate-blob ${delay}`}
       />
-      // <div
-      //    className={`absolute top-44 right-16 md:-right-20 circle-wh-72 md:circle-wh-112 ${color2} rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-2000`}
-      // />
-      // <div
-      //    className={`absolute top-8 right-0 md:left-64 circle-wh-72 md:circle-wh-112 ${color3} rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-4000`}
-      // />
    );
 }
