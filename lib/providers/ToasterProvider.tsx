f56e25a275b24d19d0082c24c407eb766042ea4b
Maria Paz <pazrodriguez612@gmail.com>
2024-11-12 13:10:40 -0800

    re formatted

--- lib/providers/ToasterProvider.tsx
+++ lib/providers/ToasterProvider.tsx
@@ -1,9 +1,9 @@
 import { Toaster } from 'react-hot-toast';
 
 export default function ToasterProvider() {
-  return (
-    <div className='z-[9999]'>
-      <Toaster />
-    </div>
-  );
+   return (
+      <div className='z-[9999]'>
+         <Toaster />
+      </div>
+   );
 }
